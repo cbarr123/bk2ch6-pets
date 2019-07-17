ec02a86bcb91f235166700d6794b83e86e26ec5d
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-07-17 11:24:02 -0500

    added functions with this

--- main.js
+++ main.js
@@ -9,13 +9,24 @@ const coco = {
     pant: function(pantFocus) {
         console.log(`${coco.nicknames[0]} is panting because she ${pantFocus}`)
     },
-    favoriteToys: ["bone", "chicken"],
+    favoriteToys: ["bone"],
     play: function() {
-        
-    }
+        let favToy = window.prompt("Enter favorite toy")
+        this.favoriteToys.push(favToy)
+    },
+    run: function () {
+        let doorStatus = window.prompt("Is the door open?")
+        if (doorStatus === "yes"){
+            console.log(`${coco.nicknames[0]} is out of here`)
+        }
+    },
     
 }
 
-
+console.log(coco.favoriteToys)
 coco.bark("dog")
 coco.pant("ran")
+coco.play()
+coco.run()
+
+console.log(coco.favoriteToys)
